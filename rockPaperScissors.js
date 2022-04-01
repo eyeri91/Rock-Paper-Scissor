e841174e22aa38b4a37d60448b52f1fcd3cddc47
Yuri The Doer <eyeri91@gmail.com>
2022-04-01 20:57:34 +0400

    Update the current score

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,62 +1,86 @@
+
+const optionBtns = document.querySelectorAll('.options');
+const comBoard = document.querySelector('.com.score');
+const myBoard = document.querySelector('.player.score');
+const leftGame = document.querySelector('.game.left');
+const winAnnounce = document.querySelector('.winner');
+const currentGame = document.querySelector('.current');
+
+optionBtns.forEach(button => { button.addEventListener('click', getPlayerSelect) });
+
+
 const options = ['Rock', 'Paper', 'Scissor']
 
 let playerScore = 0;
 let computerScore = 0;
 
+
 function computerPlay() {
     // Randomly choose the computer's option from the options array.
-    let computerChoice = 
+    let computerChoice =
         options[Math.floor(Math.random() * options.length)];
-  return computerChoice;
+    return computerChoice;
 }
 
-function capitalizeUserInput(playerInput) {
-    // Turn user input to a capitalized string.
-    playerInput = playerInput[0].toUpperCase()
-    + playerInput.slice(1).toLowerCase();
-    return playerInput;
+function updateScore(computerScore, playerScore) {
+    let comScore = 'Computer: ' + computerScore;
+    let myScore = 'Player: ' + playerScore;
+    comBoard.textContent = comScore;
+    myBoard.textContent = myScore;
 }
+
 
 function playRound(comSelect, playerSelect) {
-    // Create a variable to return the result.
-    let result;
-
-    // If result is tie, return draw.
     if (comSelect == playerSelect) {
-        return result = "draw";
-    // If User wins, increment the user score and return win.
+        currentGame.textContent = 'Draw!'
+        // updateScore(computerScore, playerScore);
     } else if (comSelect == 'Rock' && playerSelect == 'Paper' ||
-               comSelect == 'Paper' && playerSelect == 'Scissor' ||
-               comSelect == 'Scissor' && playerSelect == 'Rock') {
+        comSelect == 'Paper' && playerSelect == 'Scissor' ||
+        comSelect == 'Scissor' && playerSelect == 'Rock') {
         playerScore += 1;
-        return result = "win"; 
-    // If Computer wins, increment the computer score and return lose.           
+        // updateScore(computerScore, playerScore);
+        currentGame.textContent = 'Player won!'
     } else {
         computerScore += 1;
-        return result = "lose";
+        // updateScore(computerScore, playerScore);
+        currentGame.textContent = 'Computer won!'
+    }
+    updateScore(computerScore, playerScore);
+    // updateWinner(computerScore, playerScore);
+}
+
+function leftGameUpdate(totalRound) {
+    leftGame.textContent = totalRound;
+}
+
+function updateWinner(computerScore, playerScore) {
+    if (computerScore > playerScore) {
+        winAnnounce.textContent = 'You lost. Try again!'
+    } else {
+        winAnnounce.textContent = 'You won! Well done!'
     }
 }
 
-function game(totalRound = 5) { 
-    let gamePlayed = 0;
-    while (gamePlayed < totalRound) {
-        let player = prompt('Rock, paper or scissor?!')
-        let computer = computerPlay();
+// function game(totalRound = 5) {
+//     let gamePlayed = 0;
+//     while (gamePlayed < totalRound) {
+//         // let player = prompt('Rock, paper or scissor?!')
+//         // let computer = computerPlay();
 
-        player = capitalizeUserInput(player);
-        result = playRound(computer,player);
+//         // player = capitalizeUserInput(player);
+//         // result = playRound(computer, player);
 
-        if (result == 'win') {
-            console.log(`${player} beats ${computer}, you won.`);
-        } else if (result == 'lose') {
-            console.log(`${computer} beats ${player}, you lost.`);
-        } else {
-            console.log('Draw!');
-        }
-        gamePlayed ++;
-    }
-   displayGameResult(computerScore, playerScore);
-}
+//         if (result == 'win') {
+//             console.log(`${player} beats ${computer}, you won.`);
+//         } else if (result == 'lose') {
+//             console.log(`${computer} beats ${player}, you lost.`);
+//         } else {
+//             console.log('Draw!');
+//         }
+//         totalRound--;
+//     }
+//     displayGameResult(computerScore, playerScore);
+// }
 
 
 function displayGameResult(computerScore, playerScore) {
@@ -70,5 +94,23 @@ function displayGameResult(computerScore, playerScore) {
 }
 
 
-game(5);
+
+function getPlayerSelect(e) {
+    let comSelect = computerPlay()
+    let playerSelect = e.target.textContent;
+    playRound(comSelect, playerSelect);
+}
+
+
+
+// game(1);
+
+
+// function capitalizeUserInput(playerInput) {
+//     // Turn user input to a capitalized string.
+//     playerInput = playerInput[0].toUpperCase()
+//         + playerInput.slice(1).toLowerCase();
+//     return playerInput;
+// }
+
 
